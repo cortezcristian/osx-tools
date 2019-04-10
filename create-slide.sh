82382ce0b34bca8e9bb9135838d6f3d092d06ac5
Cristian Cortez <cortez.cristian@gmail.com>
2019-04-11 00:46:56 +0100

    Creating new slides

--- create-slide.sh
+++ create-slide.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-NEW_SLIDE=$(yo reveal:slide "$@" |grep html |tail -n 1 )
-#echo $NEW_SLIDE
+NEW_SLIDE=$(yo reveal:slide "$@" 2>&1 >/dev/null |tail -n 1 |awk '{ print $NF }')
+echo $NEW_SLIDE
+if [[ $NEW_SLIDE == *".html"* ]]; then
+  HEADER=$(cat $NEW_SLIDE | head -n 1)
+  echo $HEADER > $NEW_SLIDE
+  cat template-slide.html >> $NEW_SLIDE
+  cat $NEW_SLIDE
+fi
